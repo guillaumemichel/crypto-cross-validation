4da0150c4f0e853f9c7472ddfb156b7d6e31b852
guillaumemichel <guillaume.michel.epfl@gmail.com>
2018-11-27 19:54:28 +0100

    updated for homerwork 4, some additions will follow

--- cross-validate.sh
+++ cross-validate.sh
@@ -2,29 +2,60 @@
 
 pathtosage="/Applications/SageMath/sage"
 pathtoparameters="parameters.txt"
-filename="hw3-"
+filename="test4-"
 
 count=0
 sciper=0
 curr=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
     if [ $count -eq 0 ]; then
+        # set the sciper number as it is the value at the first line
         sciper=${line:7:6}
+
     elif [ "${line:0:6}" == "######" ]; then
+        # if "######" then new exercise => new parameters
         if [ $count -ne 1 ]; then
-            eval '$pathtosage' '$filename$curr.py' '$param'
+            # sage test4-1.py args
+            eval '$pathtosage' '$filename$curr.py' $param
         fi
+        # echo Exercise 1
         echo ${line:7:19}
+        # first parameter is always the sciper (even if it is not useful for the exercise)
         param="$sciper"
+        # set the exercise number to curr
         curr=${line:16:1}
-    elif [ "${line:2:3}" == " = " ]; then
-        param="$param ${line:5}"
-    elif [ "${line:2:1}" == "=" ]; then
-        param="$param ${line:3}"
+
+
+    elif [[ $line == *"="* ]]; then
+        # if the line contains '=', we add the parameter to the parameter's list
+        str=$(echo "$line"| cut -d'=' -f2-)
+        if [ "${str:0:1}" == " " ]; then
+            # if there is a space after the =, we truncate it
+            str=${str:1}
+        fi
+        if [ "${str:0:1}" == "\"" ]; then
+            # if the parameter is a "string": str = "12 34"
+            str="\"$(echo "$str"| cut -d'"' -f2)\""
+        elif [ "${str:0:1}" == "'" ]; then
+            # if the parameter is a 'string': str = '12 34'
+            str="'$(echo "$str"| cut -d"'" -f2)'"
+        elif [ "${str:0:1}" == "(" ]; then
+            # if the parameter is a tuple: str = "(1, 2, 3)"
+            str="\"$str\""
+        elif [ "${str:0:1}" == "[" ]; then
+            # if the parameter is an array: str = "[(1, 2), (3,4)]"
+            str="\"$str\""
+        fi
+        # add the parameter to the list
+        param="$param $str"
+
     else
+        # don't know how to handle the line => print it
         echo "$line"
+
     fi
     ((count++))
 done < $pathtoparameters
-eval '$pathtosage' '$filename$curr.py' '$param'
+# evaluate the last exercise
+eval '$pathtosage' '$filename$curr.py' $param
 
